5c51a14009f610445420a22884183e4fa60bbb38
maritoo17 <msancgim@myuax.com>
2023-12-18 12:56:41 +0100

    Ej 1) 9

--- Ejercicio 1/rand.cpp
+++ Ejercicio 1/rand.cpp
@@ -3,8 +3,9 @@
 #include <time.h>
 
 int main() {
-    int srand(time(NULL));
-    int num = rand() % 100 +1;
+    srand(time(0));
+
+    int num = rand() % 100 +1; //Coge un numero aleatorio entre 0 y 99 y le suma 1
     printf ("El numero aleatorio es %d\n", num);
 
     return 0;
