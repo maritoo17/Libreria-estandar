b26a7ff4a48f436a752ca733d0c12c3309ab606f
maritoo17 <msancgim@myuax.com>
2023-12-18 14:50:52 +0100

    Ej 3) 8

--- Ejercicio 3/fprintf.cpp
+++ Ejercicio 3/fprintf.cpp
@@ -1,1 +1,9 @@
 #include <stdio.h>
+
+int main() {
+    FILE *file = fopen("archivos.txt", "w");
+    if (file == NULL) {
+        printf("No se ha podido abrir el fichero.\n");
+        return 1;
+    }
+}
