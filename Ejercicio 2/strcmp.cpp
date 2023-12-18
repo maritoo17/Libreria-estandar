712de471f977004e2426d60145808b4bc5de510f
maritoo17 <msancgim@myuax.com>
2023-12-18 13:06:35 +0100

    Ej 2) 4

--- Ejercicio 2/strcmp.cpp
+++ Ejercicio 2/strcmp.cpp
@@ -1,9 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main() {
     char str1[] = "Antoine";
     char str2[] = "Griezmann";
 
     int comparison = strcmp(str1, str2);
+
+    if (comparison == 0) {
+        printf("Las cadenas son iguales\n");
+    } else if (comparison < 0) {
+        printf("La primera cadena es menor que la segunda\n");
+    } else {
+        printf("La primera cadena es mayor que la segunda\n");
+    }
+
+    return 0;
 }
